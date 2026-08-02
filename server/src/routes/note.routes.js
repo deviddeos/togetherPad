94e99f3db1a76dce4a9f3fc9ee6cb463561ebcdd
shobhit7098 <20prince22@gmail.com>
2026-08-02 18:37:52 +0530

    feat: implement note state retrieval API

--- server/src/routes/note.routes.js
+++ server/src/routes/note.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createNote } from "../controllers/note.controller.js";
+import { createNote, getNote } from "../controllers/note.controller.js";
 import validate from "../middleware/validate.middleware.js";
 import { createNoteSchema } from "../validators/note.validator.js";
 
 const router = Router();
 
 router.post("/", validate(createNoteSchema), createNote);
+router.get("/:slug", getNote);
 
 export default router;
